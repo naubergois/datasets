f9e8656b1f30b5806aaef3d8290173a1df81529a
Nauber <naubergois@gmail.com>
2022-08-07 16:20:25 -0300

    Adicionando dados com mongo

--- ForNode/02-fetching-all-products/util/database.js
+++ ForNode/02-fetching-all-products/util/database.js
@@ -1,17 +1,30 @@
+
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
+
+let _db;
 
 const mongoConnect = callback => {
   MongoClient.connect(
     "mongodb://localhost:27017/MyDb"
-  )
+    )
     .then(client => {
       console.log('Connected!');
-      callback(client);
+      _db = client.db();
+      callback();
     })
     .catch(err => {
       console.log(err);
+      throw err;
     });
 };
 
-module.exports = mongoConnect;
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw 'No database found!';
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
