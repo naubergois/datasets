cb20e588b8ff84f19888c68c881e4b38cb638c74
Nauber <naubergois@gmail.com>
2022-08-08 20:53:50 -0300

    for react

---
+++ ForReact/003_-_jsx/src/index.js
@@ -0,0 +1,19 @@
+// 1) Import the React and ReactDOM libraries
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+// 2) Get a reference to the div with ID root
+const el = document.getElementById('root');
+
+// 3) Tell React to take control of that element
+const root = ReactDOM.createRoot(el);
+
+// 4) Create a component
+function App() {
+  const message = {};
+
+  return <h1>{message}</h1>;
+}
+
+// 5) Show the component on the screen
+root.render(<App />);
